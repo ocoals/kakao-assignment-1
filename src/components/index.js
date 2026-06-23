6959608db2487124be983e8430b8c6a2cb33b1b6
ohchaemin <ohchaemin00@gmail.com>
2026-06-24 02:52:11 +0900

    chore: 2차 Vite 앱 제거 (과제3 풀스택 재구성, 코드는 week-03에 보존)

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-import FilterTabs from "./FilterTabs";
-import TodoForm from "./TodoForm";
-import TodoList from "./TodoList";
-import WeekStrip from "./WeekStrip";
-
-export { FilterTabs, TodoForm, TodoList, WeekStrip };
